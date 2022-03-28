8ed10cac1988372858a4b0053bbb12e7c9dffeee
Martin Lagalm <majok5040@gmail.com>
2022-03-28 11:40:07 -0600

    Authentication system and user registration

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,71 @@
+const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+
+const createToken = (user, secret, expiresIn) => {
+    const { id, email, name, lastname } = user;
+
+    return jwt.sign( { id, email, name, lastname }, secret, { expiresIn } )
+}
+
 const resolvers = {
     Query: {
-        get: () => "Get"
+        getUser: async (_, { token }) => {
+            const userId = await jwt.verify(token, process.env.SECRET);
+
+            return userId;
+        }
+    },
+    Mutation: {
+
+        registerUser: async (_, { input }) => {
+
+            const { email, password } = input;
+
+            // Check duplicate users
+            const checkUser = await User.findOne({email});
+            if(checkUser) {
+                throw new Error('Usuario registrado');
+            }
+
+            // Hash pass
+            const salt = bcryptjs.genSaltSync(10);
+            input.password = bcryptjs.hashSync(password, salt);
+
+            try {
+                // Save user
+                const user = new User(input);
+                user.save();
+                return user;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        authUser: async (_, {input}) => {
+
+            const { email, password } = input;
+
+            // Check if the user exists
+            const checkUser = await User.findOne({email});
+            if (!checkUser) {
+                throw new Error('El usuario no existe');
+            }
+
+            // Check if the pass is correct
+            const checkPass = await bcryptjs.compare( password, checkUser.password );
+            if (!checkPass) {
+                throw new Error('La contraseña es incorrecta');
+            }
+
+            // Create token
+            return {
+                token: createToken(checkUser, process.env.SECRET, '24h')
+            }
+
+        },
+
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,39 @@ const { gql } = require('apollo-server');
 
 // schema
 const typeDefs = gql`
+
+    type User {
+        id: ID
+        name: String
+        lastname: String
+        email: String
+        createdAt: String
+    }
+
+    type Token {
+        token: String
+    }
+
+    input UserInput {
+        name: String!
+        lastname: String!
+        email: String!
+        password: String!
+
+    }
+
+    input AuthInput {
+        email: String!
+        password: String!
+    }
+
     type Query {
-        get: String
+        getUser(token: String!) : User
+    }
+
+    type Mutation {
+        registerUser(input: UserInput) : User
+        authUser(input: AuthInput) : Token
     }
 `
 
